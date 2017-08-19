bd161997e3af949fc696e779fa5b5edbefbe23ad
rvrvrv <ibanez19@sbcglobal.net>
2017-08-19 10:57:58 -0500

    Expand exampleText and rename elements

--- babel/index.jsx
+++ babel/index.jsx
@@ -1,18 +1,14 @@
-const exampleText = `Heading
-=======
+const exampleText = `# Heading
 
-Sub-heading
------------
- 
+![Random image](https://unsplash.it/50 'Random image')
+
+## Sub-heading
 ### Another deeper heading
- 
+
 Paragraphs are separated
 by a blank line.
 
-Leave 2 spaces at the end of a line to do a  
-line break
-
-Text attributes *italic*, **bold**, 
+Text attributes *italic*, **bold**,
 \`monospace\`, ~~strikethrough~~ .
 
 Shopping list:
@@ -25,11 +21,21 @@ Numbered list:
 
   1. apples
   2. oranges
-  3. pears`;
+  3. pears
+
+> > This is a blockquote.
+
+\`\`\`
+// This is an example of code
+const greeting = 'Hello!';
+\`\`\`
+
+Markdown parser courtesy of [marked.js](https://github.com/chjj/marked)
+`;
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = { text: exampleText };
   }
 
@@ -38,25 +44,26 @@ class App extends React.Component {
     return { __html: marked(this.state.text, { sanitize: true }) };
   }
 
-  // Upon text change, update state and highlight marked box
+  // Upon text change, update state and highlight preview box
   handleChange = (e) => {
     this.setState({ text: e.target.value });
-    const marked = document.getElementById('marked');
-    marked.classList.add('edited');
-    setTimeout(() => marked.classList.remove('edited'), 3000);
-  }
+    const preview = document.getElementById('preview');
+    preview.classList.add("edited");
+    setTimeout(() => preview.classList.remove('edited'), 3000);
+  };
 
   render() {
     return (
       <div className="container">
         <h2 id="title"><span role="img" aria-label="document">📝</span> Markdown Previewer <span role="img" aria-label="glasses">👓</span></h2>
         <textarea
+          id="editor"
           onChange={this.handleChange}
           defaultValue={this.state.text}
           rows="30"
           cols="50"
         />
-        <p id="marked" dangerouslySetInnerHTML={this.markIt()} />
+        <p id="preview" dangerouslySetInnerHTML={this.markIt()} />
       </div>
     );
   }
