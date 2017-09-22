3def391602115d34ee1b05c854f45a6fa6d43668
rvrvrv <ibanez19@sbcglobal.net>
2017-09-22 12:31:20 -0500

    Refactor code

--- babel/index.jsx
+++ babel/index.jsx
@@ -36,7 +36,7 @@ Markdown parser courtesy of [marked.js](https://github.com/chjj/marked)
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { text: exampleText };
+    this.state = { text: exampleText, editing: false };
   }
 
   // Call markdown function from external API
@@ -46,24 +46,25 @@ class App extends React.Component {
 
   // Upon text change, update state and highlight preview box
   handleChange = (e) => {
-    this.setState({ text: e.target.value });
-    const preview = document.getElementById('preview');
-    preview.classList.add("edited");
-    setTimeout(() => preview.classList.remove('edited'), 3000);
+    this.setState({ text: e.target.value, editing: true });
   };
 
   render() {
+    const editingStyle = { boxShadow: '0 0 40px #aaf' };
     return (
-      <div className="container">
-        <h2 id="title"><span role="img" aria-label="document">📝</span> Markdown Previewer <span role="img" aria-label="glasses">👓</span></h2>
-        <textarea
-          id="editor"
-          onChange={this.handleChange}
-          defaultValue={this.state.text}
-          rows="30"
-          cols="50"
-        />
-        <p id="preview" dangerouslySetInnerHTML={this.markIt()} />
+      <div>
+        <h2 className="title"><span role="img" aria-label="document">📝</span> Markdown Previewer <span role="img" aria-label="glasses">👓</span></h2>
+        <div className="container">
+          <textarea
+            className="editor"
+            onChange={this.handleChange}
+            onBlur={() => this.setState({ editing: false })}
+            defaultValue={this.state.text}
+            rows="30"
+            cols="50"
+          />
+          <p className="preview" dangerouslySetInnerHTML={this.markIt()} style={this.state.editing ? editingStyle : null} />
+        </div>
       </div>
     );
   }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -6,7 +6,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var exampleText = '# Heading\n\n![Random image](https://unsplash.it/50 \'Random image\')\n\n## Sub-heading\n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n> > This is a blockquote.\n\n```\n// This is an example of code\nconst greeting = \'Hello!\';\n```\n\nMarkdown parser courtesy of [marked.js](https://github.com/chjj/marked)\n';
+var exampleText = "# Heading\n\n![Random image](https://unsplash.it/50 'Random image')\n\n## Sub-heading\n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\n> > This is a blockquote.\n\n```\n// This is an example of code\nconst greeting = 'Hello!';\n```\n\nMarkdown parser courtesy of [marked.js](https://github.com/chjj/marked)\n";
 
 var App = function (_React$Component) {
   _inherits(App, _React$Component);
@@ -17,15 +17,10 @@ var App = function (_React$Component) {
     var _this = _possibleConstructorReturn(this, _React$Component.call(this));
 
     _this.handleChange = function (e) {
-      _this.setState({ text: e.target.value });
-      var preview = document.getElementById('preview');
-      preview.classList.add("edited");
-      setTimeout(function () {
-        return preview.classList.remove('edited');
-      }, 3000);
+      _this.setState({ text: e.target.value, editing: true });
     };
 
-    _this.state = { text: exampleText };
+    _this.state = { text: exampleText, editing: false };
     return _this;
   }
 
@@ -38,32 +33,42 @@ var App = function (_React$Component) {
   // Upon text change, update state and highlight preview box
 
   App.prototype.render = function render() {
+    var _this2 = this;
+
+    var editingStyle = { boxShadow: '0 0 40px #aaf' };
     return React.createElement(
-      'div',
-      { className: 'container' },
+      "div",
+      null,
       React.createElement(
-        'h2',
-        { id: 'title' },
+        "h2",
+        { className: "title" },
         React.createElement(
-          'span',
-          { role: 'img', 'aria-label': 'document' },
-          '📝'
+          "span",
+          { role: "img", "aria-label": "document" },
+          "📝"
         ),
-        ' Markdown Previewer ',
+        " Markdown Previewer ",
         React.createElement(
-          'span',
-          { role: 'img', 'aria-label': 'glasses' },
-          '👓'
+          "span",
+          { role: "img", "aria-label": "glasses" },
+          "👓"
         )
       ),
-      React.createElement('textarea', {
-        id: 'editor',
-        onChange: this.handleChange,
-        defaultValue: this.state.text,
-        rows: '30',
-        cols: '50'
-      }),
-      React.createElement('p', { id: 'preview', dangerouslySetInnerHTML: this.markIt() })
+      React.createElement(
+        "div",
+        { className: "container" },
+        React.createElement("textarea", {
+          className: "editor",
+          onChange: this.handleChange,
+          onBlur: function onBlur() {
+            return _this2.setState({ editing: false });
+          },
+          defaultValue: this.state.text,
+          rows: "30",
+          cols: "50"
+        }),
+        React.createElement("p", { className: "preview", dangerouslySetInnerHTML: this.markIt(), style: this.state.editing ? editingStyle : null })
+      )
     );
   };
 
